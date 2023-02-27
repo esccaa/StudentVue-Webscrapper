c74d74879a8063ed4978fafe440e9394572bae1e
Geonwoo Kim <geonwoo@Geonwoos-Macbook-Air.local>
2023-02-26 20:37:50 -0800

    Fixed Selenium Error

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # StudentView-Application
 
-README file coming soon
+## Set Up
+
+In the folder StudentView-Application would be downloaded in, create a new file named "id_password.txt"
+You can create the new file inside the terminal with :
+```
+> touch id_password.txt
+```
+
+Inside the file, write your Student ID on the first line, and write your Password on the second line :
+```
+Your_ID
+Your_StudentView_Password
+```
--- StudentView_Webscrapper.py
+++ StudentView_Webscrapper.py
@@ -2,9 +2,16 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from pathlib import Path
 import requests
 from bs4 import BeautifulSoup
 import time
+
+
+def delete_last_element(string1) :
+	return string1[:-1]
+
+
 
 startTime = time.time()
 
@@ -12,8 +19,6 @@ classNum = 0
 classNames = []
 classOverallGrades = []
 courseTitles = []
-
-
 
 #  Hide Pop-Up Window
 
@@ -25,11 +30,11 @@ chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
 
 #  Login
 
-passwordFile = open("id_password.txt", "r")
-passwordFileContents = passwordFile.readlines()
-id_num = passwordFileContents[1]
-password_num = passwordFileContents[2]
-passwordFile.close()
+id_password_file = open("id_password.txt", "r")
+id_num = delete_last_element(str(id_password_file.readline()))
+password_num = delete_last_element(str(id_password_file.readline()))
+id_password_file.close()
+
 
 
 browser = webdriver.Chrome(options=chrome_options)
@@ -37,10 +42,11 @@ browser.get("https://parentvue.cusdk8.org/PXP2_Login_Student.aspx?regenerateSess
 id_search_box = browser.find_element("id", "ctl00_MainContent_username")
 password_search_box = browser.find_element("id", "ctl00_MainContent_password")
 login_click = browser.find_element("id", "ctl00_MainContent_Submit1")
-
 id_search_box.send_keys(id_num)
 password_search_box.send_keys(password_num)
 login_click.click()
+
+
 
 
 #  Gradebook Data
@@ -50,18 +56,18 @@ gradeBookSoup = BeautifulSoup(browser.page_source, "lxml")
 
 templist = gradeBookSoup.find_all("button", attrs={"class":"btn btn-link course-title"})
 classNum = len(templist)
-for i in range(len(templist)) :
+for i in range(classNum) :
 	courseTitles.append(templist[i].get_text())
 
 templist = []
 templist = gradeBookSoup.find_all("span", attrs={"class":"score"})
-for i in range(len(templist)) :
+for i in range(classNum) :
 	classOverallGrades.append(templist[i].get_text())
 
 
 
-
-
+for i in range(classNum) :
+	print(courseTitles[i], ":", classOverallGrades[i], "percent")
 
 
 
@@ -72,4 +78,5 @@ for i in range(len(templist)) :
 browser.close()
 
 endTime = time.time()
+print("\n")
 print("Time Elapsed :", endTime-startTime, "seconds")
