a80739fa07256ac6e7799c1b0c63d32c4810539c
Geonwoo Kim <gwkim973@gmail.com>
2023-04-21 11:03:00 -0700

    got grade data parsed

--- src/all.js
+++ src/all.js
@@ -1,17 +1,22 @@
 const StudentVue = require('studentvue.js');
 const fs = require('fs');
-fs.readFile(__dirname+'/id_password.txt', (err, inputD) => {
-    if (err) throw err;
-        seperated = inputD.toString().split(" ");
-        gotLoginInfo(seperated[0], seperated[1], seperated[2]);
-})
+
+fs.readFile(__dirname + '/id_password.txt', (err, inputD) => {
+  if (err) throw err;
+  const separated = inputD.toString().split(' ');
+  gotLoginInfo(separated[0], separated[1], separated[2]);
+});
 
 function gotLoginInfo(districtURL, id, password) {
-    StudentVue.login(districtURL, id, password)
-        .then(client => {
-            gradebookRaw = client.getGradebook();
-        })
-        .catch(error => {
-            console.error(error);
-        });
+  StudentVue.login(districtURL, id, password)
+    .then(client => client.getGradebook())
+    .then(grades => {
+      const gradesRaw = grades;
+      const parsedGrades = JSON.parse(gradesRaw);
+      function gotParsed(parsedGrades);
+    });
+}
+
+function gotParsed(gradeJSON) {
+    
 }
